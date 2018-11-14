d332cf7faa87105caf44ecc5aec34ae38bba9781
Valase Ionut <ionutvalase@gmail.com>
2018-11-14 21:05:32 +0200

    Added Login Screen

---
+++ app/src/main/java/com/example/ionut/androidcourse/LoginActivity.java
@@ -0,0 +1,51 @@
+package com.example.ionut.androidcourse;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private EditText etUserName;
+    private EditText etPassword;
+    private String registeredUser = "Ionut";
+    private String userPassword = "google";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btnLogin);
+        etUserName = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userNameFromForm = etUserName.getText().toString();
+                String passwordFromForm = etPassword.getText().toString();
+                Log.d("LoginActivity", "Login pressed");
+                if (userNameFromForm.equals(registeredUser)) {
+                    if (passwordFromForm.equals(userPassword)) {
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, "Wrong Password", Toast.LENGTH_LONG).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Wrong Username", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+
+        });
+    }
+}
